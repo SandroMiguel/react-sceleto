1a2d7bc317413b4f8a4fa9cf3f9ef073a16c7610
SandroMiguel <sandromiguel@sandromiguel.com>
2024-04-19 16:46:23 +0100

    feat: setup eslint-config-cecilia

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,1 @@
-module.exports = {
-    "extends": "airbnb",
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-      }
-};
+module.exports = { extends: "cecilia" }
